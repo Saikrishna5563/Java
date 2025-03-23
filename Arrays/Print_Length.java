fe71a3bb430645a1290ff5a417a946280c5758ba
Leela Sai krishna Velpuri <leelasaikrishnavelpuri@gmail.com>
2025-03-23 18:57:56 +0530

    Create Print_Length.java

---
+++ Arrays/Print_Length.java
@@ -0,0 +1,16 @@
+/*
+ * To find the Array Length
+ */
+
+public class Print_Length {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr []= {20,30,40,50,60};
+		int size=arr.length;
+		System.out.println("length of the array >>>"+size);
+      
+
+	}
+
+}
