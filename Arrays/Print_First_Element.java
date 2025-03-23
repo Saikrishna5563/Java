e8099709acdb9936cc0efe4f70ad8217dc1f4d48
Leela Sai krishna Velpuri <leelasaikrishnavelpuri@gmail.com>
2025-03-23 19:00:14 +0530

    Create Print_First_Element.java
    Print First Element in Arrays

---
+++ Arrays/Print_First_Element.java
@@ -0,0 +1,9 @@
+public class Print_First_Element {
+
+	public static void main(String[] args) {
+		int [] arr= {20,30,40,50,60};
+		// print first element of array;
+		System.out.println("First_element is : "+arr[0]);
+	}
+
+}
